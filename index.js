9fde5d2e517138926ba68f1e313ae3d887bd78b6
bobbyhadz <opmftw@gmail.com>
2024-03-05 08:36:43 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+console.log('bobbyhadz.com');
+
+const box = document.getElementById('box');
+
+// ✅ Remove CSS properties
+box.style.removeProperty('width');
+box.style.removeProperty('height');
+box.style.removeProperty('background-color');
+
+// ✅ Set CSS Properties
+// box.style.setProperty('background-color', 'salmon');
